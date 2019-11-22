e5f6cab5fa73ca701e5841cd3377035081f3245b
s-liujef <s-liujef@53S50031104.it.bsd405.org>
2019-11-21 22:41:09 -0800

    Fraction Calculator Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -7,35 +7,71 @@ public class FracCalc {
 
     public static void main(String[] args) {
     	
-    Scanner s = new Scanner(System.in);
-    System.out.print("Enter Your Fraction: ");
-    String userInput = s.nextLine();
+    Scanner fractions = new Scanner(System.in);
+    System.out.println("Enter Your Fraction Equation: ");
+    String input = fractions.nextLine();
     
-    System.out.println(produceAnswer(userInput));
-    
+    while (!input.equals("quit")) {
+    	System.out.println(produceAnswer(input));
+    	System.out.println("Enter Your Fraction Equation: ");
+    	input = fractions.nextLine();
     }
-        // TODO: Read the input from the user and call produceAnswer with an equation
-    
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
+    fractions.close();
+    }
+        
     public static String produceAnswer(String input) {
-    	Scanner s = new Scanner(input);
-    	String fraction1 = s.next();
-    	String operator = s.next();
-    	String fraction2 = s.next();
     	
-    	return fraction2;
-       }
+    	String math = input;
+    	String fraction1 = math.substring(0, math.indexOf(' '));
+    	math = math.substring(math.indexOf(' ') + 1);
+    	String mathSymbol = math.substring(0, math.indexOf(' '));
+    	math = math.substring(math.indexOf(' ') + 1);
+    	String fraction2 = math;
+    	
+    	String whole2 = whole2(fraction2);
+    	String numerator2 = numerator2(fraction2);
+    	String denominator2 = denominator2(fraction2);
+    	
+    	String fractionsFor2 = "whole:" + whole2 + " numerator:" + numerator2 + " denominator:" + denominator2;
+    	
+      
+    return fractionsFor2;
+    }
+   
+	public static String whole2(String input) {
+    	if(input.contains("_")) {
+    		return input.substring(0, input.indexOf('_'));
+    	}
+    	else if(input.contains("/")) {
+    		return "0";
+    	}
+    	else return input;
+    	}
+   
     
-}
+    public static String numerator2(String input) {
+    	if(input.contains("_")) {
+    		return input.substring(input.indexOf('_') + 1, input.indexOf('/'));
+    	}
+    	else if(input.contains("/")) {
+    		return input.substring(0, input.indexOf('/'));
+    	}
+    	else {
+    		return "0";
+    	}
+    }
+   
+    public static String denominator2(String input) {
+    	if(input.contains("_")) {
+    		return input.substring(input.indexOf("/") + 1);
+    	}
+    	else if(input.contains("/")) {
+    		return input.substring(input.indexOf('/') + 1);
+    	}
+    	else {
+    		return "1";
+    	}
+    }
+    }
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
-
-
+ 
