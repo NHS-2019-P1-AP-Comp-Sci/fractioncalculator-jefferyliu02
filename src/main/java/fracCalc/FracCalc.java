005a8a2125523b09f4450f4ce6d86dd0ca51781b
s-liujef <s-liujef@53S50031104.it.bsd405.org>
2019-12-09 23:38:02 -0800

    Final Turn In

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -1,44 +1,41 @@
-
+// Author: Jeffery Liu
 package fracCalc;
 
 import java.util.Scanner;
+import java.lang.Math;
 
-public class FracCalc {
-
+public class FracCalc { // sets variables
+	private static int sign1;
 	private static int whole1;
 	private static int numerator1;
 	private static int denominator1;
 
+	private static int sign2;
 	private static int whole2;
 	private static int numerator2;
 	private static int denominator2;	
 	
-	// private static int whole3;
+	//private static int whole3;
 	private static int numerator3;
 	private static int denominator3;
+	
     public static void main(String[] args) {
     	
-    Scanner fractions = new Scanner(System.in);
+    Scanner fractions = new Scanner(System.in); //Takes user input with ability to quit program
     System.out.println("Enter Your Fraction Equation: ");
     String input = fractions.nextLine();
     
     while (!input.equals("quit")) {
-    	char operator = findOperator(input);
+    	String result = produceAnswer(input);
    
-    	if (operator == ' ')
+    	if (result.isEmpty())
     	{
     		System.out.println("Invalid expression!");
         	System.out.println("Enter Your Fraction Equation: ");  
         	input = fractions.nextLine();
     		continue;
     	}
-    	int opIndex = input.indexOf(operator);
-    	
-    	String operand1 = produceAnswer(input.substring(0, opIndex-1), true);
-    	String operand2 = produceAnswer(input.substring(opIndex+1), false);
-    	//System.out.println(operand1);
-    	//System.out.println(operand2);
-    	String result = calculate(operator);
+   	
     	System.out.println("Result = " + result);
     	System.out.println("Enter Your Fraction Equation: ");
     	input = fractions.nextLine();
@@ -47,45 +44,70 @@ public class FracCalc {
     
     }
 
+    
+    public static String produceAnswer(String input) // Parses through user input to find num, denom, op
+    {                                                // Performs arithmetic on isolated numerator, denominator
+    	char operator = findOperator(input);
+    	   
+    	if (operator == ' ')
+    	{
+    		return "";
+    	}
+    	int opIndex = input.indexOf(" " + operator + " ");
+    	
+    	setOperand(input.substring(0, opIndex), true);
+    	setOperand(input.substring(opIndex+3), false);
+    	return calculate(operator);    	
+    }
+    
     public static char findOperator(String input)
     {
-    	int opIndex = input.indexOf('+');
-    	if (opIndex >= 0)
+    	int opIndex = input.indexOf(" + ");
+    	if (opIndex > 0)
     	{
     		return '+';
     	}
-    	opIndex = input.indexOf('-');
-    	if (opIndex >= 0)
+    	opIndex = input.indexOf(" - ");
+    	if (opIndex > 0)
     	{
     		return '-';
     	}   
-    	opIndex = input.indexOf('*');
+    	opIndex = input.indexOf(" * ");
     	if (opIndex >= 0)
     	{
     		return '*';
     	}
-    	opIndex = input.indexOf(" /");
+    	opIndex = input.indexOf(" / ");
     	if (opIndex >= 0)
     	{
     		return '/';
     	}	
     	return ' ';
     }
-    public static String produceAnswer(String input, Boolean isFirst) {
-    	String math = input;
-    	
-    	String fraction = math;
-    	String whole = whole1(fraction);
-    	String numerator = numerator1(fraction);
-    	String denominator = denominator1(fraction);
+    
+    public static String setOperand(String operand, Boolean isFirst) {
+   	
+    	System.out.println("operand = " + operand);
+    	int sign = 1;
+    	if (operand.startsWith("-"))
+    	{
+    		sign = -1;
+    		operand = operand.substring(1);
+    	}
+    	String whole = whole1(operand);
+    	String numerator = numerator1(operand);
+    	String denominator = denominator1(operand);
+    	System.out.println("Sign:" +  sign + " whole:" + whole + " numerator:" + numerator + " denominator:" + denominator);    	
     	if (isFirst)
     	{
+    		sign1 = sign;
     		whole1 = Integer.parseInt(whole.trim());
     		numerator1 = Integer.parseInt(numerator.trim());	
     		denominator1 = Integer.parseInt(denominator.trim());	
     	}
     	else
     	{
+    		sign2 = sign;
     		whole2 = Integer.parseInt(whole.trim());
     		numerator2 = Integer.parseInt(numerator.trim());	
     		denominator2 = Integer.parseInt(denominator.trim());    		
@@ -104,8 +126,9 @@ public class FracCalc {
     	else if(input.contains("/")) {
     		return "0";
     	}
-    	else return input;
-    	}
+    	else 
+    		return input;
+    }
    
     
     public static String numerator1(String input) {
@@ -132,12 +155,13 @@ public class FracCalc {
     	}
     }
     public static String calculate(char operator) {
-    	int n1 = whole1 * denominator1 + numerator1;
-    	int n2 = whole2 * denominator2 + numerator2;
+    	int n1 = sign1 * (whole1 * denominator1 + numerator1);
+    	int n2 = sign2 * (whole2 * denominator2 + numerator2);
     	
     	if(operator == '+') {
     		numerator3 = (n1 * denominator2) + (n2 * denominator1);
     		denominator3 = (denominator1 * denominator2);
+    		//System.out.println("numerator3 = " + numerator3 + "denominator3=" + denominator3); // use for troubleshooting
     	}
     	else if(operator == '-') {
     		numerator3 = (n1 * denominator2) - (n2 * denominator1);
@@ -151,13 +175,60 @@ public class FracCalc {
     	else if(operator == '/') {
     		numerator3 = (n1 * denominator2);
     		denominator3 = (n2 * denominator1);
+    		if (numerator3 < 0 && denominator3 < 0)
+    		{
+    			numerator3 = Math.abs(numerator3);
+    			denominator3 = Math.abs(denominator3);	
+    		}
+    		else if (numerator3 > 0 && denominator3 < 0)
+    		{
+    			numerator3 = -1 *numerator3;
+    			denominator3 = Math.abs(denominator3);	    			
+    		}
     	}
     	else {
     		return "Expression has invalid operator";
     	}
-    	return numerator3 + "/" + denominator3;
+    	// turns fraction into reduced format
+    	int i = 2;
+    	while(i <= Math.min(Math.abs(numerator3), Math.abs(denominator3))) {
+    		if((numerator3 % i == 0 && denominator3 % i == 0)) {
+    			numerator3 = numerator3 / i;
+    			denominator3 = denominator3 /i;
+    			i = 2;
+    		}
+    		else {
+    			i++;
+    		}
+    	}
+    	if(numerator3 == denominator3) { // turns answer back into mixed number or other proper format
+    		return "1";
+    	}
+    	else if(numerator3 == 0) {
+    		return "0";
+    	}
+    	else if(Math.abs(numerator3) > Math.abs(denominator3)) {
+    		if (numerator3 % denominator3 == 0)
+    		{
+    			return "" + numerator3 / denominator3;
+    		}
+    		else if (numerator3 / denominator3 < 0)
+    		{
+    			return (numerator3 / denominator3) + "_" + -1*(numerator3 % denominator3) + "/" + denominator3;
+    		}
+    		else
+    		{
+    			return (numerator3 / denominator3) + "_" + (numerator3 % denominator3) + "/" + denominator3;   			
+    		}
+    	} 
+    	else if(Math.abs(numerator3) < Math.abs(denominator3)) {
+    		return numerator3 + "/" + denominator3;
+    	}
+    	return "";
     }
-   
 }
+    
+
+
 
  
